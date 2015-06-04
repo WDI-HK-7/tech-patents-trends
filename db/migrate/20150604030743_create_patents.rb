d0c428514c7906f117087c428621047eb08c45e0
youngyang <yyangag@gmail.com>
2015-06-04 02:02:17 -0700

    patents table updated

--- db/migrate/20150604030743_create_patents.rb
+++ db/migrate/20150604030743_create_patents.rb
@@ -2,19 +2,8 @@ class CreatePatents < ActiveRecord::Migration
   def change
     create_table :patents do |t|
       t.string :company_full_name
-      t.integer :yr2014
-      t.integer :yr2013
-      t.integer :yr2012
-      t.integer :yr2011
-      t.integer :yr2010
-      t.integer :yr2009
-      t.integer :yr2008
-      t.integer :yr2007
-      t.integer :yr2006
-      t.integer :yr2005
-      t.integer :yr2004
-      t.integer :yr2003
-      t.integer :yr2002
+      t.integer :patent_number
+      t.integer :year
       t.timestamps null: false
     end
     add_reference :patents, :company, index: true, foreign_key: true
